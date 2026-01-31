f09f9946ea728fea260d8bb1f8f846fdfbb1ab14
Praveen <pamidinagapraveen585@gmail.com>
2026-01-31 11:13:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ app.secret_key = os.environ.get("SECRET_KEY", "dev-secret-key")
 MONGO_URI = os.environ.get("MONGO_URI")
 
 if not MONGO_URI:
-    raise Exception("MONGO_URI environment variable not set")
+    print("Warning: MONGO_URI not set")
 
 client = MongoClient(MONGO_URI)
 db = client.online_exam
